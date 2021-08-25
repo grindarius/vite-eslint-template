48e652be8493bb52daf1e17d4e9d4310091fcbb0
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-08-25 12:32:24 +0700

    Initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+import rootPage from '../App.vue'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Root',
+    component: rootPage
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+export default router
