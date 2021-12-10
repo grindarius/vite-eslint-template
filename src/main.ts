b41f54f24c5969b792a82e6d2a51187c5da986ed
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-12-11 01:43:31 +0700

    install vue-meta

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
+import { createMetaManager } from 'vue-meta'
 
 import App from './App.vue'
 import Router from './router'
 
-createApp(App).use(Router).mount('#app')
+createApp(App).use(Router).use(createMetaManager()).mount('#app')
