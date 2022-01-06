2ea3a2564aa8551c46b74a33a4a10f6975602d4f
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-01-06 13:56:42 +0700

    update versions

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
-import { createMetaManager } from 'vue-meta'
 
 import App from './App.vue'
 import Router from './router'
 
-createApp(App).use(Router).use(createMetaManager()).mount('#app')
+createApp(App).use(Router).mount('#app')
